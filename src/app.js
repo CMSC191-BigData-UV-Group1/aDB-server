ab3b5123e5b136c88f323df0dd9e94b8638eba91
septa97 <jancharles.adona@gmail.com>
2017-03-16 23:48:00 +0800

    Added body-parser and compression middleware

--- src/app.js
+++ src/app.js
@@ -6,10 +6,17 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import chalk from 'chalk';
+import body_parser from 'body-parser';
+import compression from 'compression';
 
 // Create server
 const app = express();
 const server = http.createServer(app);
+
+// Use Middlewares
+app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.json());
+app.use(compression());
 
 // Use CORS
 app.use(cors());
